735fbda6f2d02b06701e181f03fc0c050de8498a
Plamen Tanov <ptanov@setelis.com>
2015-12-14 14:43:35 +0200

    initial version

---
+++ test/spec/seEventHelperServiceTest.spec.js
@@ -0,0 +1,11 @@
+describe("SeEventHelperService", function () {
+	"use strict";
+
+	beforeEach(module("seEventHelperService", function() {
+	}));
+	beforeEach(inject(function () {
+	}));
+
+	it("no-tests-yet", inject(function () {
+	}));
+});
